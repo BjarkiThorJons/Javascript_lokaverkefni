669663351f2c7b2af82c7bb32136e70ce63ae0b0
BjarkiThorJons <bjarkithorjonsson@gmail.com>
2018-05-02 13:38:04 +0000

    vinna með JSON

--- lokaverk/script.js
+++ lokaverk/script.js
@@ -3,8 +3,14 @@ $.ajax({
   'type': 'GET',
   'dataType': 'json',
   'success': function(response) {
-  	for (var i = 0; i < response.results.length; i++) {			
-		console.log(response.results[i].eventDateName);
-	};	
+  	let test = {}
+  	for (let i = 0; i < response.results.length; i++) {			
+		test[response.results[i].eventDateName] = {date : response.results[i].dateOfShow, stadur : response.results[i].eventHallName, mynd : response.results[i].imageSource};
+	};
+	console.log(test);
+	for (let key in test){
+		let hlutur = test[key]
+		document.getElementById("syningar").innerHTML +='<h2>'+key+'</h2><img src='+hlutur.mynd+' />';
+	}
   }
 });
